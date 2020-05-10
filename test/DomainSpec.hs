b3b5e5f957c4142bdd6a321091a3138bcf4468a4
Mahler, Thomas <thomas.mahler@ista.com>
2020-05-10 15:18:38 +0200

    adding testcases

--- test/DomainSpec.hs
+++ test/DomainSpec.hs
@@ -10,9 +10,9 @@ main :: IO ()
 main = hspec spec
 
 day = fromGregorian 2020 1 29
-reservation1 = Reservation day "Andrew M. Jones" "amjones@example.com" 4
-reservation2 = Reservation day "Thomas Miller" "tm@example.com" 3
-list = [reservation1, reservation2]
+res1 = Reservation day "Andrew M. Jones" "amjones@example.com" 4
+res2 = Reservation day "Thomas Miller" "tm@example.com" 3
+reservations = [res1, res2]
 totalCapacity = 20
 
 spec :: Spec
@@ -22,19 +22,23 @@ spec =
       usedCapacity [] `shouldBe` 0
 
     it "computes the used capacity for a list of reservations" $
-      usedCapacity [reservation1, reservation2] `shouldBe` 7
+      usedCapacity [res1, res2] `shouldBe` 7
 
     it "can check if a reservation is possible on a given day" $ 
-      isReservationPossible (Reservation day "name" "mail@mail.com" 8) list  totalCapacity `shouldBe` True
+      isReservationPossible (Reservation day "name" "mail@mail.com" 8) reservations  totalCapacity `shouldBe` True
 
     it "can check if a reservation is possible on a day with no bookings" $ 
       isReservationPossible (Reservation day "name" "mail@mail.com" 8) []    totalCapacity `shouldBe` True
 
     it "detects if a reservation is not possible on a given day" $ 
-      isReservationPossible (Reservation day "name" "mail@mail.com" 15) list totalCapacity `shouldBe` False
+      isReservationPossible (Reservation day "name" "mail@mail.com" 15) reservations totalCapacity `shouldBe` False
 
     it "can add a reservation to a list of reservations" $ do
-      addReservation reservation1 [] `shouldBe` [reservation1]
-      addReservation reservation1 list `shouldBe` reservation1:list
+      addReservation res1 [] `shouldBe` [res1]
+      addReservation res1 reservations `shouldBe` res1:reservations
 
-
+    it "can cancel a reservation" $ do
+      cancelReservation res1 reservations `shouldBe` [res2]
+      cancelReservation res1 [] `shouldBe` []
+      cancelReservation res2 reservations `shouldBe` [res1]
+      cancelReservation res2 (cancelReservation res1 reservations) `shouldBe` []
