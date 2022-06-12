97134f54cc92e8651c644ab0b6dffc75ff4eb22f
thma <gH1710>
2022-06-12 10:14:11 +0200

    disable tracing for unit tests

--- test/InterfaceAdaptersRestServiceSpec.hs
+++ test/InterfaceAdaptersRestServiceSpec.hs
@@ -38,7 +38,7 @@ liftServer config = hoistServer reservationAPI (interpretServer config) reservat
         & runKvsAsSQLite
         & runInputConst config
         & runError @ReservationError
-        & traceToIO
+        & ignoreTrace 
         & runM
         & liftToHandler
     liftToHandler = Handler . ExceptT . (fmap handleErrors)
