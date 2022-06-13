c7e8a033416d1339f6cf08ca02edee076d591802
Mahler, Thomas <thomas.mahler@ista.com>
2022-06-13 10:23:07 +0200

    litte cleanup

--- src/InterfaceAdapters/KVSAcidState.hs
+++ src/InterfaceAdapters/KVSAcidState.hs
@@ -17,11 +17,9 @@ import           Data.SafeCopy
 import           Data.Typeable
 import           Polysemy
 import           UseCases.KVS         (KVS (..))
---import           Data.Acid.Advanced
 
 newtype KeyValue k v = KeyValue (Map.Map k v) deriving Typeable
 
--- $(deriveSafeCopy 0 'base ''KeyValue)
 instance (Ord k, SafeCopy k, Typeable k, SafeCopy v, Typeable v) => SafeCopy (KeyValue k v) where
     putCopy (KeyValue state) = contain $ safePut state
     getCopy = contain $ fmap KeyValue safeGet
@@ -88,60 +86,3 @@ insertAction key val = do
 --  update acid (DeleteKey key)
 --  closeAcidState acid
 --  return []
-
-
-------------------------------------------------------
--- The gritty details. These things may be done with
--- Template Haskell in the future.
-
---data InsertKey k v = InsertKey k v
---newtype LookupKey k v = ConLookupKey k -- v
---
---
---deriving instance Typeable InsertKey
---instance (Typeable k, SafeCopy k, Typeable v, SafeCopy v) => SafeCopy (InsertKey k v) where
---    putCopy (InsertKey key value) = contain $ safePut key >> safePut value
---    getCopy = contain $ InsertKey <$> safeGet <*> safeGet
---instance (Typeable k, Typeable v) => Method (InsertKey k v) where
---    type MethodResult (InsertKey k v) = ()
---    type MethodState (InsertKey k v) = KeyValue k v
---instance (Typeable k, Typeable v) => UpdateEvent (InsertKey k v)
---
---deriving instance Typeable LookupKey
---instance (Typeable k, SafeCopy k, Typeable v, SafeCopy v) => SafeCopy (LookupKey k v) where
---    putCopy (ConLookupKey key) = contain $ safePut key
---    getCopy = contain $ ConLookupKey <$> safeGet -- <*> safeGet
---instance (Typeable k, SafeCopy k, Typeable v, SafeCopy v) => Method (LookupKey k v) where
---    type MethodResult (LookupKey k v) = Maybe v
---    type MethodState (LookupKey k v) = KeyValue k v
---instance (Typeable k, SafeCopy k, Typeable v, SafeCopy v) => QueryEvent (LookupKey k v)
---
---instance (Typeable k, SafeCopy k, Ord k, Typeable v, SafeCopy v) => IsAcidic (KeyValue k v) where
---    acidEvents = 
---      [ UpdateEvent (\(InsertKey key value) -> insertKey key value) safeCopyMethodSerialiser
---      , (QueryEvent (\(ConLookupKey key)   -> lookupKey key))       safeCopyMethodSerialiser
---      ]
-
-{--
-deleteUser :: Id -> Handler ()
-deleteUser id = do
-  liftIO $ putStrLn $ "DELETE /users/" ++ id
-  eitherVoidEx <- liftIO $ try (delete userType id) :: Handler (Either PersistenceException ())
-  case eitherVoidEx of
-    Left ex -> throwAsServerError ex
-    Right v -> return v
-  where
-    userType = Proxy :: Proxy User
-    
--- | delete an entity of type a and identified by an Id to a json file
-delete :: Proxy a -> Id -> IO ()
-delete proxy id = do
-    -- compute file path based on runtime type and entity id
-    let jsonFileName = getPath (typeRep proxy) id
-    fileExists <- doesFileExist jsonFileName
-    if fileExists
-      then removeFile jsonFileName
-      else throw $ EntityNotFound ("could not delete as entity was not found: " ++ jsonFileName)
-    
-
---}
