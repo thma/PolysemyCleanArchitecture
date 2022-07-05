1a18030f6134f191249fe754da83f28845973733
thma <gH1710>
2022-07-05 21:09:15 +0200

    at least it compiles

--- src/InterfaceAdapters/ConfigProvider.hs
+++ src/InterfaceAdapters/ConfigProvider.hs
@@ -3,9 +3,15 @@
 module InterfaceAdapters.ConfigProvider where
 
 import InterfaceAdapters.Config
-import Polysemy (makeSem)
+import Polysemy
+import Polysemy.Internal
 
 data ConfigProvider m a where
-  GetConfig :: ConfigProvider m Config
+  GetConfig :: String -> ConfigProvider m Config
 
 makeSem ''ConfigProvider
+
+{--
+getConfig :: Member ConfigProvider r => String -> Sem r Config
+getConfig x = send (GetConfig x :: ConfigProvider (Sem r) Config)
+--}
--- src/InterfaceAdapters/StaticConfigProvider.hs
+++ src/InterfaceAdapters/StaticConfigProvider.hs
@@ -4,10 +4,14 @@ import InterfaceAdapters.Config
 import InterfaceAdapters.ConfigProvider
 import Polysemy (Embed, Member, Sem, embed, interpret)
 
-runSimpleConfigProvider :: (Member (Embed IO) r) => Sem (ConfigProvider : r) a -> Sem r a
-runSimpleConfigProvider = interpret $ \case
-  getConfig -> embed loadConfig
+--
+runSimpleConfigProvider :: (Member (Embed IO) r) => String -> Sem (ConfigProvider : r) a -> Sem r a
+runSimpleConfigProvider name = interpret $ \case
+  GetConfig confName -> embed loadConfig
+--}
 
 -- | load application config. In real life, this would load a config file or read commandline args.
 loadConfig :: IO Config
 loadConfig = return Config {port = 8080, backend = SQLite, dbPath = "kvs.db", verbose = True}
+
+
